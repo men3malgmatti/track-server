1118844654f903af78bc94a0a7ec97ccbab01b7d
onedamianocoder <damy32dc@gmail.com>
2021-04-16 18:21:21 +0200

    add user

---
+++ src/index.js
@@ -0,0 +1,36 @@
+require("./models/User")
+const express = require("express")
+const mongoose = require("mongoose")
+const authRoutes= require("./routes/authRoutes")
+const bodyParser= require("body-parser")
+const requireAuth = require("./middlewares/requireAuth");
+
+const app = express() 
+
+app.use(bodyParser.json())
+app.use(authRoutes)
+
+const mongoUri= "mongodb+srv://men3malgmatti:adminadmin@cluster0.rmh9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+mongoose.connect(mongoUri,{
+   useNewUrlParser:true,
+   useCreateIndex:true 
+});
+
+mongoose.connection.on("connected",()=>{
+    console.log("Connected to mongo instance");
+})
+
+mongoose.connection.on("error",(err)=>{
+    console.error("Error connecting to mongo",err);
+})
+
+app.get("/",requireAuth,(req,res)=>{
+    res.send(`your email ${req.user.email}`)
+})
+
+
+
+app.listen(3001, ()=>{
+    console.log("listening on port 3001");
+})
